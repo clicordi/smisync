a3cc69f666281f350532325e68fc83495802d6ac
maxdestors <max.destors@gmail.com>
2013-08-27 20:00:35 +0200

    Update script de la synchronisation/reparation de la dase de données dolibarr dans celle de smi

--- scripts/SmiSync_repair.php
+++ scripts/SmiSync_repair.php
@@ -93,172 +93,207 @@ try {
     // on boucle pour chaque clients
     while($userDoli = $usersDoli->fetch(PDO::FETCH_BOTH)) 
     {
-      
+    
         // on tri les clients dans les tiers
     	if($userDoli['client'] != 0)
     	{
             //on verifie si notre client est present dans la table des correspondances
-            if(isset($idcli[$userDoli['rowid']]))
+            // et on test si notre client est dans la bdd smi
+            if(isset($idcli[$userDoli['rowid']]) && isset($cliSmi[$idcli[$userDoli['rowid']]]))
             {
                 $idSmi = $idcli[$userDoli['rowid']];
                 echo 'present id correspondance';
+
+                //on test les valeurs de notre client smi avec celles de doli
                 $err = 0;
-                
-                 if(isset($cliSmi[$idSmi]))
+                echo 'ligne bdd smi';
+                if($cliSmi[$idSmi]['cli_cat'] != 'PAR')
                 {
-                    echo 'ligne bdd smi';
-                    if($cliSmi[$idSmi]['cli_cat'] != 'PAR')
-                    {
-                        $cliSmi[$idSmi]['cli_cat'] = 'PAR';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_codecrea'] != 'Administrateur')
-                    {
-                        $cliSmi[$idSmi]['cli_codecrea'] = 'Administrateur';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_prop'] != 'A06530')
-                    {
-                        $cliSmi[$idSmi]['cli_prop'] = 'A06530';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_codemod'] != 'Administrateur')
-                    {
-                        $cliSmi[$idSmi]['cli_codemod'] = 'Administrateur';
-                        $err = 1;
-                    }
-                    /*if($cliSmi[$idSmi]['cli_code'] != 'C12')
-                    {
-                        $cliSmi[$idSmi]['cli_code'] = 'C12';
-                        $err = 1;
-                    }*/
-                    if($cliSmi[$idSmi]['cli_pass'] != '')
-                    {
-                        $cliSmi[$idSmi]['cli_pass'] = '';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_type'] != '2')
-                    {
-                        $cliSmi[$idSmi]['cli_type'] = '2';
-                        $err = 1;
-                    }
-                
-                    if($userDoli['civilite'] == 'MME' && $cliSmi[$idSmi]['cli_civilite'] != 'MME')
-                    {
-                        echo 'civ0 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
-                        $cliSmi[$idSmi]['cli_civilite'] = 'MME';
-                        $err = 1;
-                        
-                    }
-                    else if($userDoli['civilite'] == 'MLE' && $cliSmi[$idSmi]['cli_civilite'] != 'MELLE') 
-                    {
-                        echo 'civ1 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
-                        $cliSmi[$idSmi]['cli_civilite'] = 'MELLE';
-                        $cli_civilite = 'MELLE';  // modifier cette valeur en MME si l'on ne veux pas insulter les madames
-                        $err = 1;
-                        
-                    }
-                    else if($cliSmi[$idSmi]['cli_civilite'] != 'M.')
-                    {
-                        echo 'civ2 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
-                        $cliSmi[$idSmi]['cli_civilite'] = 'M.';
-                        $err = 1;
-                        
-                    }
-
-                    if($cliSmi[$idSmi]['cli_prenom'] != $userDoli['nom'])
-                    {
-                        echo 'nom '.$cliSmi[$idSmi]['cli_prenom'] .' / '.$userDoli['nom'];
-                        $cliSmi[$idSmi]['cli_prenom'] = $userDoli['nom'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_nom'] != ' ')
-                    {
-                        $cliSmi[$idSmi]['cli_nom'] = ' ';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_adr1'] != $userDoli['address'])
-                    {
-                        $cliSmi[$idSmi]['cli_adr1'] = $userDoli['address'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_adr2'] != $userDoli['zip'].' '.$userDoli['town'])
-                    {
-                        $cliSmi[$idSmi]['cli_adr2'] = $userDoli['zip'].' '.$userDoli['town'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_dep'] != substr($userDoli['zip'], 0, 2))
-                    {
-                        $cliSmi[$idSmi]['cli_dep'] = substr($userDoli['zip'], 0, 2);
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_ville'] != '0')
-                    {
-                        $cliSmi[$idSmi]['cli_ville'] = '0';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_codepays'] != 'FR')
-                    {
-                        $cliSmi[$idSmi]['cli_codepays'] = 'FR';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_codeadev'] != 'EUR')
-                    {
-                        $cliSmi[$idSmi]['cli_codeadev'] = 'EUR';
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_telf'] != $userDoli['phone'])
-                    {
-                        $cliSmi[$idSmi]['cli_telf'] = $userDoli['phone'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_fax'] != $userDoli['fax'])
-                    {
-                        $cliSmi[$idSmi]['cli_fax'] = $userDoli['fax'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_telp'] != $userDoli['phone_mobile'])
-                    {
-                        $cliSmi[$idSmi]['cli_telp'] = $userDoli['phone_mobile'];
-                        $err = 1;
-                    }
-                    if($cliSmi[$idSmi]['cli_email'] != $userDoli['email'])
-                    {
-                        $cliSmi[$idSmi]['cli_email'] = $userDoli['email'];
-                        $err = 1;
-                    }
-                    
-                    
-                    if($err == 1)
-                    {
-                        $myquery = 'UPDATE smi_cli SET';
-                        foreach($cliSmi[$idSmi] as $key => $val) 
-                        {
-                            $myquery .= ' '. $key .'=\''. $val .'\',';
-                        }
-                        $myquery = substr($myquery, 0, -1);
-                        $myquery .= ' WHERE cli_id = '. $idSmi;
-                        //$myquery = "UPDATE smi_cli SET cli_datemod='$cli_datemod', cli_civilite='$cli_civilite', cli_prenom='$cli_prenom', cli_adr1='$cli_adr1', cli_adr2='$cli_adr2', cli_dep='$cli_dep', cli_telf='$cli_telf', cli_fax='$cli_fax', cli_telp='$cli_telp', cli_email='$cli_email' WHERE cli_id = $cliSmiId";
-                        //$myquery = "UPDATE smi_cli SET cli_cat='$cli_datemod', cli_civilite='$cli_civilite', cli_prenom='$cli_prenom', cli_adr1='$cli_adr1', cli_adr2='$cli_adr2', cli_dep='$cli_dep', cli_telf='$cli_telf', cli_fax='$cli_fax', cli_telp='$cli_telp', cli_email='$cli_email' WHERE cli_id = $cliSmiId";
-                        echo '<br>'.$myquery.'<br>';
-                    }
-                    
-
+                    $cliSmi[$idSmi]['cli_cat'] = 'PAR';
+                    $err = 1;
                 }
+                if($cliSmi[$idSmi]['cli_codecrea'] != 'Administrateur')
+                {
+                    $cliSmi[$idSmi]['cli_codecrea'] = 'Administrateur';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_prop'] != 'A06530')
+                {
+                    $cliSmi[$idSmi]['cli_prop'] = 'A06530';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_codemod'] != 'Administrateur')
+                {
+                    $cliSmi[$idSmi]['cli_codemod'] = 'Administrateur';
+                    $err = 1;
+                }
+                /*if($cliSmi[$idSmi]['cli_code'] != 'C12')
+                {
+                    $cliSmi[$idSmi]['cli_code'] = 'C12';
+                    $err = 1;
+                }*/
+                if($cliSmi[$idSmi]['cli_pass'] != '')
+                {
+                    $cliSmi[$idSmi]['cli_pass'] = '';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_type'] != '2')
+                {
+                    $cliSmi[$idSmi]['cli_type'] = '2';
+                    $err = 1;
+                }
+            
+                if($userDoli['civilite'] == 'MME' && $cliSmi[$idSmi]['cli_civilite'] != 'MME')
+                {
+                    echo 'civ0 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
+                    $cliSmi[$idSmi]['cli_civilite'] = 'MME';
+                    $err = 1;
+                    
+                }
+                else if($userDoli['civilite'] == 'MLE' && $cliSmi[$idSmi]['cli_civilite'] != 'MELLE') 
+                {
+                    echo 'civ1 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
+                    $cliSmi[$idSmi]['cli_civilite'] = 'MELLE';
+                    $cli_civilite = 'MELLE';  // modifier cette valeur en MME si l'on ne veux pas insulter les madames
+                    $err = 1;
+                    
+                }
+                else if($cliSmi[$idSmi]['cli_civilite'] != 'M.')
+                {
+                    echo 'civ2 '.$cliSmi[$idSmi]['cli_civilite'] .' / '.$userDoli['civilite'];
+                    $cliSmi[$idSmi]['cli_civilite'] = 'M.';
+                    $err = 1;
+                    
+                }
+
+                if($cliSmi[$idSmi]['cli_prenom'] != $userDoli['nom'])
+                {
+                    echo 'nom '.$cliSmi[$idSmi]['cli_prenom'] .' / '.$userDoli['nom'];
+                    $cliSmi[$idSmi]['cli_prenom'] = $userDoli['nom'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_nom'] != ' ')
+                {
+                    $cliSmi[$idSmi]['cli_nom'] = ' ';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_adr1'] != $userDoli['address'])
+                {
+                    $cliSmi[$idSmi]['cli_adr1'] = $userDoli['address'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_adr2'] != $userDoli['zip'].' '.$userDoli['town'])
+                {
+                    $cliSmi[$idSmi]['cli_adr2'] = $userDoli['zip'].' '.$userDoli['town'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_dep'] != substr($userDoli['zip'], 0, 2))
+                {
+                    $cliSmi[$idSmi]['cli_dep'] = substr($userDoli['zip'], 0, 2);
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_ville'] != '0')
+                {
+                    $cliSmi[$idSmi]['cli_ville'] = '0';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_codepays'] != 'FR')
+                {
+                    $cliSmi[$idSmi]['cli_codepays'] = 'FR';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_codeadev'] != 'EUR')
+                {
+                    $cliSmi[$idSmi]['cli_codeadev'] = 'EUR';
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_telf'] != $userDoli['phone'])
+                {
+                    $cliSmi[$idSmi]['cli_telf'] = $userDoli['phone'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_fax'] != $userDoli['fax'])
+                {
+                    $cliSmi[$idSmi]['cli_fax'] = $userDoli['fax'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_telp'] != $userDoli['phone_mobile'])
+                {
+                    $cliSmi[$idSmi]['cli_telp'] = $userDoli['phone_mobile'];
+                    $err = 1;
+                }
+                if($cliSmi[$idSmi]['cli_email'] != $userDoli['email'])
+                {
+                    $cliSmi[$idSmi]['cli_email'] = $userDoli['email'];
+                    $err = 1;
+                }
+                
+                
+                if($err != 0)
+                {// une erreur dans les données
+                    $myquery = 'UPDATE smi_cli SET';
+                    foreach($cliSmi[$idSmi] as $key => $val) 
+                    {
+                        $myquery .= ' '. $key .'=\''. $val .'\',';
+                    }
+                    $myquery = substr($myquery, 0, -1);
+                    $myquery .= ' WHERE cli_id = '. $idSmi;
+                    //$myquery = "UPDATE smi_cli SET cli_cat='$cli_datemod', cli_civilite='$cli_civilite', cli_prenom='$cli_prenom', cli_adr1='$cli_adr1', cli_adr2='$cli_adr2', cli_dep='$cli_dep', cli_telf='$cli_telf', cli_fax='$cli_fax', cli_telp='$cli_telp', cli_email='$cli_email' WHERE cli_id = $cliSmiId";
+                    echo '<br>'.$myquery.'<br>';
+                }
+                else
+                {//pas d'erreur dans les données
+                    echo 'Client sans Probleme';
+                }
+
+
+                /*{}
                 else
                 {
                      echo 'pas ligne bdd smi';
-                     //Client present dans la table des correspondance mais pas dans smi
+                     //Client dans la table des correspondance mais pas dans smi
                      //donc faire un INSERT
-                }
-                /*
-                //$cli_datecrea = date('Y-m-d'); // date du jour
-                //$cli_datemod = date('Y-m-d'); // date du jour
+                }*/
+            }
+            else
+            {
+                
+                echo 'client pas present dans smi';
+                
+                $cli_cat = 'PAR'; // PAR pour 'particulier'
+                $cli_datecrea = date('Y-m-d'); // date du jour
+                $cli_codecrea = 'Administrateur';
+                $cli_datemod = date('Y-m-d'); // date du jour
+                $cli_prop = 'A06530'; // code du 'magasin' (possibilité de le recuperer quelque part dans la bdd)
+                $cli_codemod = 'Administrateur';
+                $cli_code++; // on incremente le code client
+                $cli_pass = '';
+                $cli_type = '2'; // 2 pour 'Deja client' - 1 pour demande d'intervention - 0 pour prospect
                 $cli_ste = '';
                 $cli_rcs = '';
                 $cli_ape = '';
                 $cli_tvai = '';
-
+    
+                if($userDoli['civilite'] == 'MME')
+                    $cli_civilite = 'MME';
+                else if($userDoli['civilite'] == 'MLE')
+                    $cli_civilite = 'MELLE';  // modifier cette valeur en MME si l'on ne veux pas insulter les madames
+                else
+                    $cli_civilite = 'M.';
+    
+                $cli_prenom = addslashes($userDoli['nom']); // mis dans le champ prenom pour raison de formatage de text dans le champ nom
+                $cli_nom = ' ';
+                $cli_adr1 = addslashes($userDoli['address']);
+                $cli_adr2 = addslashes($userDoli['zip']).' '.addslashes($userDoli['town']); // code pour la ville (2 ligne en dessous) un peu special je le met donc dans ce champ
+                $cli_dep = addslashes(substr($userDoli['zip'], 0, 2)); // recupere le num de departement dans le code postale
+                // a voir
+                $cli_ville = '0';
+                $cli_codepays = 'FR';
+                $cli_codeadev = 'EUR';
+                $cli_telf = addslashes($userDoli['phone']);
+                $cli_fax = addslashes($userDoli['fax']);
+                $cli_telp = addslashes($userDoli['phone_mobile']);
+                $cli_email = addslashes($userDoli['email']);
                 $cli_mess = '';
                 $cli_notaa = '';
                 $cli_notat = '';
@@ -270,19 +305,21 @@ try {
                 
                 // j'insert le client dolibarr dans la bdd de smi
                 $myquery = "INSERT INTO smi_cli (cli_cat, cli_datecrea, cli_codecrea, cli_datemod, cli_prop, cli_codemod, cli_code, cli_pass, cli_type, cli_ste, cli_rcs, cli_ape, cli_tvai, cli_civilite, cli_prenom, cli_nom, cli_adr1, cli_adr2, cli_dep, cli_ville, cli_codepays, cli_codeadev, cli_telf, cli_fax, cli_telp, cli_email, cli_mess, cli_notaa, cli_notat, cli_ccpta, cli_ccptasp, cli_cpta, cli_prev, cli_modfact) VALUES ('$cli_cat', '$cli_datecrea', '$cli_codecrea', '$cli_datemod', '$cli_prop', '$cli_codemod', '$cli_code', '$cli_pass', '$cli_type', '$cli_ste', '$cli_rcs', '$cli_ape', '$cli_tvai', '$cli_civilite', '$cli_prenom', '$cli_nom', '$cli_adr1', '$cli_adr2', '$cli_dep', '$cli_ville', '$cli_codepays', '$cli_codeadev', '$cli_telf', '$cli_fax', '$cli_telp', '$cli_email', '$cli_mess', '$cli_notaa', '$cli_notat', '$cli_ccpta', '$cli_ccptasp', '$cli_cpta', '$cli_prev', '$cli_modfact')";
-                echo $myquery.'<br>';
+                echo '<br>'.$myquery.'<br>';
                 $bddSmi->query($myquery);
-                */
-                $cptModif = 0;
-                //UPDATE
-            }
-            else
-            {
-                 echo 'pas present id correspondance';
-               //INSERT dans la table des correspondances et dans la table client smi
                 
+                if(!isset($idcli[$userDoli['rowid']]))
+                {
+                    echo 'Id créé dans la table de correspondance<br>';
+                    $lastSmiId1 = $bddSmi->query("SELECT LAST_INSERT_ID() FROM smi_cli");
+                    $lastSmiId0 = $lastSmiId1->fetch(PDO::FETCH_BOTH);
+                    $lastSmiId =  $lastSmiId0[0];
+                    
+                    $this->db->query("INSERT INTO llx_idcli (idcli_doli, idcli_smi) VALUES (".$userDoli['rowid'].", $lastSmiId)");
+                }
+
+
             }
-            
         
     	}
     
