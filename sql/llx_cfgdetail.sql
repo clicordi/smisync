ccd076a51f9ce3e2bc85ecc4b8f59f847d1f95a5
Max Destors <max.destors@gmail.com>
2013-09-09 18:11:19 +0200

    Modification sans consequence

--- sql/llx_cfgdetail.sql
+++ sql/llx_cfgdetail.sql
@@ -1,8 +1,8 @@
 create table llx_cfgdetail
 (
-  cfgdetail_rowid       integer AUTO_INCREMENT PRIMARY KEY,
-  cfgdetail_column   varchar(255) NOT NULL,
-  cfgdetail_label       varchar(255) NOT NULL,
-  cfgdetail_table       varchar(255) NOT NULL,
-  cfgdetail_display    integer DEFAULT 1 NOT NULL
+	cfgdetail_rowid integer AUTO_INCREMENT PRIMARY KEY,
+	cfgdetail_column varchar(255) NOT NULL,
+	cfgdetail_label varchar(255) NOT NULL,
+	cfgdetail_table varchar(255) NOT NULL,
+	cfgdetail_display integer DEFAULT 1 NOT NULL
 )ENGINE=innodb;
--- sql/llx_idcli.sql
+++ sql/llx_idcli.sql
@@ -1,5 +1,5 @@
 CREATE TABLE llx_idcli
 (
-idcli_doli int(11) unsigned NOT NULL,
-idcli_smi int(10) unsigned NOT NULL
+	idcli_doli int(11) unsigned NOT NULL,
+	idcli_smi int(10) unsigned NOT NULL
 );
